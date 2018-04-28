5413d4c6c4d9f140d37e7732dae9c3432105dff9
Lybron Sobers <lybron.developer@gmail.com>
2018-04-27 22:38:34 -0400

    Walkthrough completed

--- src/main.cpp
+++ src/main.cpp
@@ -284,6 +284,9 @@ int main() {
                   // ref_vel = 29.5; // mph
 
                   too_close = true;
+                  if (lane > 0) {
+                    lane = 0;
+                  }
                 }
               }
             }
